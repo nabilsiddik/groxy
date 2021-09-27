25712c5bcb70cf060ce602778d94c07771c1eba8
nabilsiddik <56316655+nabilsiddik@users.noreply.github.com>
2021-09-28 00:22:11 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$('#menu').slicknav();
+
+// slick slider for testimonial section
+$('.testimonial-slick-slider').slick({
+    slidesToShow: 2,
+});
+
+// slick slider for brand section
+$('.brand-logo-slider').slick({
+    slidesToShow: 4,
+    autoplay: true
+});
+
